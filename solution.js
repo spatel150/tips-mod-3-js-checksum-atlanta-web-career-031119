ab72221369464b4792b35cce7483888999c72f15
Samidh Patel <spatel1345@gmail.com>
2019-05-08 14:28:58 -0400

    completed checkSum JS challenge

--- solution.js
+++ solution.js
@@ -1,1 +1,23 @@
+const data = [
+    [409, 194, 207, 470, 178],
+    [454, 235, 333, 511, 103],
+    [474, 293, 525, 372, 408],
+    [428, 4321, 2786, 6683, 3921],
+    [265, 262, 6206, 2207, 5712]
+  ]
+  
 
+function checkSum(data) {
+
+    let num = Math.floor(Math.abs(num))
+
+    while (num > 0) {
+        checkSum += num % 10;
+        num = Math.floor(num / 10)
+    }
+
+    return checkSum;
+
+}
+
+checkSum(data)
